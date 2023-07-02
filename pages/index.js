7df1887dc8a7e5a09436bd1c717088e276434257
System Administrator <root@Sanjays-MacBook-Air.local>
2023-07-02 09:18:48 +0530

    Committer: System Administrator <root@Sanjays-MacBook-Air.local>
      On branch main
      Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   pages/_app.js
    	modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,93 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { 
+  Web3Button,
+  useContract, 
+  useContractRead,
+  useNFTs,
+  ThirdwebNftMedia,
+  useAddress,
+ } from "@thirdweb-dev/react";
+import { useState } from 'react';
 import styles from "../styles/Home.module.css";
+import { Input } from 'antd';
+const { Search } = Input
+const FormControls = ({onSubmit}) => {
+    return (
+        <Search placeholder='Enter Word' onSearch={onSubmit} enterButton
+        style={{
+            width: 300,
+        }}/>
+    )
+      }
+//const contractAddressNFT = "0x9BC2Fc92EF2cE31a03De0df3375439376b391b7C"
+const contractAddressNFT = "0xDbf89Cf00dA32466a0cB095826F314Dae437Bc72"
+const contractAddressPT = "0x37E8BE627817a3A5ef82cF27f0df000F29A083B6"
+const tokenAmount = 1;
+
 
 export default function Home() {
+  const { contract } = useContract(contractAddressNFT);
+  const { contractPT } = useContract(contractAddressPT, "PT");
+
+  const { data: nfts, isLoading, error } = useNFTs(contract);
+  const { data: contractName } = useContractRead(contract, "name");
+  const { data: contractNamePT } = useContractRead(contractPT, "name");
+
+  const address = useAddress()
+  //console.log(address);
+ // console.log(contractNamePT);
+
+ const [uniqueWord, setUniqueWord] = useState(null)
+ const onSubmit = (values) => {
+     setUniqueWord(values)
+ }
+
+  const metadata = {
+    name: uniqueWord,
+    description: "This is a cool NFT",
+    image: ("https://portal.thirdweb.com/img/thirdweb-logo-transparent-white.svg"), // This can be an image url or file
+  }
+
+  const metadataWithSupply = {
+    metadata,
+    supply: 1, // The number of this NFT you want to mint
+
+  }
+
   return (
+    
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+          Welcome to <a href="https://thirdweb.com/">WordMint</a>!
+
         </h1>
 
         <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.js</code> file!
+          Get started by creating a new word!
         </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
+        <div>
+        <div style={{width: '100%', margin: 'auto'}}></div>
+              <FormControls onSubmit={onSubmit}/>
         </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
+        <div className={styles.code}>
+          <Web3Button
+            contractAddress={contractAddressNFT}   
+            action={(contract) => {
+              contract.erc1155.mint(metadataWithSupply)
+              }}
           >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+              Mint a Word
+          </Web3Button>
+        </div>
+        <div className={styles.code}>
+          <Web3Button
+            contractAddress={contractAddressPT}
+            action={(contractPT) =>
+              contractPT.erc20.mint(tokenAmount)
+            }
+          >
+            Mint Coins
+          </Web3Button>
         </div>
       </main>
     </div>
